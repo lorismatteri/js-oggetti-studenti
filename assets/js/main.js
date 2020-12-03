5287e5cf54725041e6f96a7df5be0e02e97a89df
Loris Matteri <loris.matteri93@gmail.com>
2020-12-03 17:02:15 +0100

    Oggetto + oggetto di oggetti
    inserito oggetto studente con proprietà poi stampate in console.
    inserito oggetto con oggetti studente con proprietà, poi stampate in console proprietà nome e cognome

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
-console.log('ciao');
+//Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+var studente = {
+  nome: 'Pinco',
+  cognome: 'Pallino',
+  eta: 25
+}
+
+for (var key in studente) {
+  console.log(key);
+}
+//Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var studenti = {
+  studente1: {
+    nome: 'Pinco',
+    cognome: 'Pallino',
+    eta: 25
+  },
+  studente2: {
+    nome: 'Zlatan',
+    cognome: 'Ibrahimovic',
+    eta: 39
+  },
+  studente3: {
+    nome: 'Romolo',
+    cognome: 'Remo',
+    eta: 25
+  }
+}
+
+for (var key in studenti) {
+  var nomeStudente = studenti[key].nome;
+  var cognomeStudente = studenti[key].cognome;
+  console.log(nomeStudente + ' ' + cognomeStudente);
+}
+//Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
